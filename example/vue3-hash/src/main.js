ca45a640b1c715b3d34cf6b6eae96ed29881b888
wangtao3 <wangtao3@izuche.com>
2024-03-21 14:24:49 +0800

    包名修改

--- example/vue3-hash/src/main.js
+++ example/vue3-hash/src/main.js
@@ -1,10 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { init } from "@js-wtao/browser";
+import { vuePlugin } from "@js-wtao/vue";
 
 import './assets/main.css'
 
 const app = createApp(App)
+
+const MitoInstance = init({
+    // set debug true to convenient debugger in dev,set false in prod
+    debug:true,
+    vue: app,
+    dsn: 'https://test.com/yourInterface',
+    maxBreadcrumbs: 100
+},[vuePlugin])
 
 app.use(router)
 
